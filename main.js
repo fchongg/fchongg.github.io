90e259183c8269e416ec79312f59ff77a91297d5
fchongg <fchongg@gmail.com>
2017-04-12 12:18:31 -0700

    Added new support

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $('.nav').click(function() {
     var id = $(this).attr('id');
+    console.log("THIS IS THE ID", id);
     $('html, body').animate({
         scrollTop: ($('#' + id + '.section').offset().top)
     }, 1000);
