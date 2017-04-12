dd2c5bd0f2fd0668d88ef049048c6fd6d40f7391
fchongg <fchongg@gmail.com>
2017-04-12 12:26:47 -0700

    Added the appropriated heare

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
-$('.nav').click(function() {
-    var id = $(this).attr('id');
-    console.log("THIS IS THE ID", id);
-    $('html, body').animate({
-        scrollTop: ($('#' + id + '.section').offset().top)
-    }, 1000);
+$(document).ready(function () {
+
+    $('.nav').click(function() {
+        var id = $(this).attr('id');
+        console.log("THIS IS THE ID", id);
+        $('html, body').animate({
+            scrollTop: ($('#' + id + '.section').offset().top)
+        }, 1000);
+    });
 });
