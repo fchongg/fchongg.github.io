d9a8f151143196be52d631a548daf226e3f9e50b
fchongg <fchongg@gmail.com>
2017-04-12 19:54:14 -0700

    removed dynamic movement

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
     $('.nav').click(function() {
         var id = $(this).attr('id');
-        console.log("THIS IS THE ID", id);
         $('html, body').animate({
             scrollTop: ($('#' + id + '.section').offset().top)
         }, 1000);
