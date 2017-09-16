10dc4e8396cf38a2009d252b1da877c9e1e1cf1b
fchongg <fchongg@gmail.com>
2017-09-16 01:11:47 -0700

    Corrected paths 	modified:   css/main.css 	modified:   index.html 	deleted:    scripts/main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $('.nav').click(function() {
-        var id = $(this).attr('id');
-        $('html, body').animate({
-            scrollTop: ($('#' + id + '.section').offset().top)
-        }, 1000);
-    });
-});
