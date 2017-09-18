b699c9c0918569a061248b2254c4919c9bb70a67
fchongg <fchongg@gmail.com>
2017-09-17 19:54:55 -0700

    Updated button and part of skills seciton

--- presentational-only/presentational-only.js
+++ presentational-only/presentational-only.js
@@ -9,7 +9,7 @@ $(document).ready(function()
 
     if( $('#more-content').hasClass('shown') )
     {
-      $('#load-more-content').text('Hide content');
+      $('#load-more-content').text('Hide information');
       $('#more-content').fadeIn('slow', function()
       {
         $('#load-more-content').fadeIn('slow');
@@ -17,7 +17,7 @@ $(document).ready(function()
     }
     else
     {
-      $('#load-more-content').text('Load some content');
+      $('#load-more-content').text('Learn more about me');
       $('#more-content').fadeOut('slow', function()
       {
         $('#load-more-content').fadeIn('slow');
